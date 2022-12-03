63444d9b7b98549214964fdb94ea713d8d598a39
RDer <kirrotle@gmail.com>
2022-12-03 17:25:58 +0800

    [New] Two Sum

---
+++ LeedCodeCSharp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LeedCodeCSharp
+{
+    public class Test
+    {
+        public static void Main(string[] args)
+        {
+            TwoSum twoSum = new TwoSum();
+        }
+    } 
+}
