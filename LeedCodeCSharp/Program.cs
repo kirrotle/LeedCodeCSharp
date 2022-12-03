bca1db4b25e13461b7acba53ec69690008fda0a1
RDer <kirrotle@gmail.com>
2022-12-03 20:04:23 +0800

    [New] 2. Add Two Numbers

--- LeedCodeCSharp/Program.cs
+++ LeedCodeCSharp/Program.cs
@@ -4,7 +4,7 @@
     {
         public static void Main(string[] args)
         {
-            TwoSum twoSum = new TwoSum();
+            AddTwoNumbers twoSum = new AddTwoNumbers();
         }
     } 
 }
